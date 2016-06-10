81c27d1973d10cb5a4db70791b73a3ac56ae5c53
Sarrg <j.kieslich@aon.at>
2016-06-10 13:28:29 +0200

    Fixed orbiting around the Sun in different angles

--- code/project/main.js
+++ code/project/main.js
@@ -7,28 +7,22 @@
     var sunTransformationNode = null;
     var context;
     var resources;
-var animatedAngle = 0;
-    var planetPosition = new Float32Array([
-   0,0,0,
-   3,0,0,
-   6,0,0,
-   9,0,0,
-   16,0,0,
-   26,0,0,
-   40,0,0
+    var animatedAngle = 0;
+    var planetDistance = new Float32Array([
+   0, 5, 6, 9, 16, 26, 40
  ]);
  var planetRotation = new Float32Array([
    0,0,0,
-   30,10,30,
-   21,12,10,
-   9,15,7,
-   16,5,22,
-   40,0,20,
-   23,23,20,
+   0,0,90,
+   0,0,-45,
+   0,75,7,
+   0,280,22,
+   -30,210,20,
+   0,300,20,
  ]);
 
     var planetSize = new Float32Array([2, 0.12, 0.20, 0.35, 0.36, 1.2, 0.90]);
-
+    var planetTransformationNodes = [];
     //var moons = new Float32Array([0, 0, 0, 1, 2, 67, 62, 27, 14]);
 
 /**
@@ -65,10 +59,11 @@ function render(timeInMilliseconds) {
   //clear the buffer
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  sunTransformationNode.matrix = glm.rotateY(-timeInMilliseconds*0.05);
+  //sunTransformationNode.matrix = glm.rotateY(-timeInMilliseconds*0.05);
+  updatePlanetTransformations(timeInMilliseconds);
   const context = createSGContext(gl);
   //context.projectionMatrix = mat4.perspective(mat4.create(), 30, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.01, 100);
-  context.viewMatrix = mat4.lookAt(mat4.create(), [-5,-25,-50], [0,0,0], [0,1,0]);
+  context.viewMatrix = mat4.lookAt(mat4.create(), [-0,-40,1], [0,0,0], [0,1,0]);
   rootNode.render(context);
   //request another call as soon as possible
   requestAnimationFrame(render);
@@ -93,21 +88,29 @@ function createSolarSystem(resources, rootNode){
     ]);
   }
   // Sun
-  sunTransformationNode = new TransformationSGNode(mat4.create(), createSphere());
-  rootNode.append(sunTransformationNode);
-  //sphereNode = createSphere();
-
+  //sunTransformationNode = new TransformationSGNode(mat4.create(), createSphere());
+  //rootNode.append(sunTransformationNode);
+  //planetTransformationNodes.push(sunTransformationNode);
   // Planets
-  for(i = 1; i < planetSize.length; i++){
-    var scale = planetSize[i];
-    var planetTransformationMatrix = mat4.multiply(mat4.create(), mat4.create(), glm.rotateX(planetRotation[i*3]));
-    planetTransformationMatrix = mat4.multiply(mat4.create(), planetTransformationMatrix, glm.rotateY(planetRotation[i*3+1]));
-    planetTransformationMatrix = mat4.multiply(mat4.create(), planetTransformationMatrix, glm.rotateZ(planetRotation[i*3+2]));
-    planetTransformationMatrix = mat4.multiply(mat4.create(), planetTransformationMatrix, glm.translate(planetPosition[i*3],planetPosition[i*3 + 1], planetPosition[i*3 + 2]));
-    planetTransformationMatrix = mat4.multiply(mat4.create(), planetTransformationMatrix, glm.scale(scale, scale, scale));
-    planetTransformationNode = new TransformationSGNode(planetTransformationMatrix);
-    sunTransformationNode.append(planetTransformationNode);
-    //sphereNode = new SphereRenderNode();
+  for(i = 0; i < planetSize.length; i++){
+  //for(i = 1; i < 2; i++){
+    planetTransformationNode = new TransformationSGNode(mat4.create(), createSphere());
+    rootNode.append(planetTransformationNode);
     planetTransformationNode.append(createSphere());
+    planetTransformationNodes.push(planetTransformationNode);
+  }
+}
+
+function updatePlanetTransformations(timeInMilliseconds){
+    for(i = 0; i < planetSize.length; i++){
+    var transformation = mat4.create();
+      var scale = planetSize[i];
+      transformation = mat4.multiply(mat4.create(), transformation, glm.rotateX(planetRotation[i*3]));
+      transformation = mat4.multiply(mat4.create(), transformation, glm.rotateY(planetRotation[i*3+1]));
+      transformation = mat4.multiply(mat4.create(), transformation, glm.rotateZ(planetRotation[i*3+2]));
+      transformation = mat4.multiply(mat4.create(), transformation, glm.rotateY(timeInMilliseconds*-0.03))
+      transformation = mat4.multiply(mat4.create(), transformation, glm.translate(planetDistance[i],0, 0));
+      transformation = mat4.multiply(mat4.create(), transformation, glm.scale(scale, scale, scale));
+      planetTransformationNodes[i].matrix = transformation;
   }
 }

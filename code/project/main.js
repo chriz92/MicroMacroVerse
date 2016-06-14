b2d57dd818a36bd0315b82c009d26cbfca627e14
chriz92 <chr_huber@gmx.at>
2016-06-14 15:03:13 +0200

    add skybox + env shader

--- code/project/main.js
+++ code/project/main.js
@@ -6,6 +6,7 @@
     var sunTransformationNode = null;
     var context;
     var resources;
+    var envcubetexture;
     var animatedAngle = 0;
     var planetDistance = new Float32Array([
    0, 5, 6, 9, 16, 26, 40
@@ -27,7 +28,7 @@
         y: 0
       },
       pos:{
-        x: 40,
+        x: -40,
         y: 0
       }
     };
@@ -38,13 +39,21 @@
       fs: 'shader/empty.fs.glsl',
       tex_vs: 'shader/texture.vs.glsl',
       tex_fs: 'shader/texture.fs.glsl',
+      env_vs: 'shader/envmap.vs.glsl',
+      env_fs: 'shader/envmap.fs.glsl',
       sunTex: "textures/sun.jpg",
       planet1Tex: "textures/planet1.jpg",
       planet2Tex: "textures/planet2.jpg",
       planet3Tex: "textures/planet3.jpg",
       planet4Tex: "textures/planet4.jpg",
       planet5Tex: "textures/planet5.jpg",
-      planet6Tex: "textures/planet6.jpg"
+      planet6Tex: "textures/planet6.jpg",
+      env_pos_x: 'textures/Galaxy_RT.jpg',
+      env_neg_x: 'textures/Galaxy_LT.jpg',
+      env_pos_y: 'textures/Galaxy_DN.jpg',
+      env_neg_y: 'textures/Galaxy_UP.jpg',
+      env_pos_z: 'textures/Galaxy_FT.jpg',
+      env_neg_z: 'textures/Galaxy_BK.jpg',
     }).then(function (resources /*an object containing our keys with the loaded resources*/) {
       init(resources);
       //render one frame
@@ -60,13 +69,14 @@ function init(resources) {
   gl = createContext();
 
   initTextures(resources);
-
+  initCubeMap(resources);
   gl.enable(gl.DEPTH_TEST);
   //compile and link shader program
   program = createProgram(gl, resources.vs, resources.fs);
   scene = 1;
   rootNode = new ShaderSGNode(createProgram(gl, resources.tex_vs, resources.tex_fs)); //TODO: global shaders (phong)
   //rootNode.append( new ShaderSGNode(createProgram(gl, resources.vs, resources.fs)));
+  rootNode.append(new ShaderSGNode(createProgram(gl, resources.env_vs, resources.env_fs)));
   switch(scene){
     case 1:
       createSolarSystem(resources, rootNode);
@@ -77,6 +87,33 @@ function init(resources) {
       break;
   }
   initInteraction(gl.canvas);
+}
+
+function initCubeMap(resources) {
+  //create the texture
+  envcubetexture = gl.createTexture();
+  //define some texture unit we want to work on
+  gl.activeTexture(gl.TEXTURE0);
+  //bind the texture to the texture unit
+  gl.bindTexture(gl.TEXTURE_CUBE_MAP, envcubetexture);
+  //set sampling parameters
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+  //gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.MIRRORED_REPEAT); //will be available in WebGL 2
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  //set correct image for each side of the cube map
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);//flipping required for our skybox, otherwise images don't fit together
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, resources.env_pos_x);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, resources.env_neg_x);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, resources.env_pos_y);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, resources.env_neg_y);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, resources.env_pos_z);
+  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, resources.env_neg_z);
+  //generate mipmaps (optional)
+  gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+  //unbind the texture again
+  gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
 }
 
 function initTextures(resources){
@@ -160,9 +197,19 @@ function render(timeInMilliseconds) {
   //animate based on elapsed time
 }
 
+function createSphere(){
+  //return //new ShaderSGNode(createProgram(gl, resources.vs, resources.fs), [
+    return new RenderSGNode(makeSphere(1,30,30));
+  //]);
+}
 
 function createSolarSystem(resources, rootNode){
   {
+    {
+      var skybox = new EnvironmentSGNode(envcubetexture, 4, false, new RenderSGNode(makeSphere(50)));
+      //rootNode.append(skybox);
+    }
+
     //initialize light
     let light = new LightSGNode(); //use now framework implementation of light node
     light.ambient = [0.2, 0.2, 0.2, 1];
@@ -179,11 +226,9 @@ function createSolarSystem(resources, rootNode){
     rootNode.append(rotateLight);
   }
 
-  function createSphere(){
-    //return //new ShaderSGNode(createProgram(gl, resources.vs, resources.fs), [
-      return new RenderSGNode(makeSphere(1,30,30));
-    //]);
-  }
+
+
+
 
   // Adding the Sun and all Planets
   for(i = 0; i < planetSize.length; i++){
@@ -215,5 +260,35 @@ function updatePlanetTransformations(timeInMilliseconds){
       transformation = mat4.multiply(mat4.create(), transformation, glm.scale(scale, scale, scale));
       transformation = mat4.multiply(mat4.create(), transformation, glm.rotateY(planetRotation[i]*timeInMilliseconds));
       planetTransformationNodes[i].matrix = transformation;
+  }
+}
+
+class EnvironmentSGNode extends SGNode {
+
+  constructor(envtexture, textureunit, doReflect , children ) {
+      super(children);
+      this.envtexture = envtexture;
+      this.textureunit = textureunit;
+      this.doReflect = doReflect;
+  }
+
+  render(context)
+  {
+    //set additional shader parameters
+    let invView3x3 = mat3.fromMat4(mat3.create(), context.invViewMatrix); //reduce to 3x3 matrix since we only process direction vectors (ignore translation)
+    gl.uniformMatrix3fv(gl.getUniformLocation(context.shader, 'u_invView'), false, invView3x3);
+    gl.uniform1i(gl.getUniformLocation(context.shader, 'u_texCube'), this.textureunit);
+    gl.uniform1i(gl.getUniformLocation(context.shader, 'u_useReflection'), this.doReflect)
+
+    //activate and bind texture
+    gl.activeTexture(gl.TEXTURE0 + this.textureunit);
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, this.envtexture);
+
+    //render children
+    super.render(context);
+
+    //clean up
+    gl.activeTexture(gl.TEXTURE0 + this.textureunit);
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
   }
 }
---
+++ code/project/shader/envmap.fs.glsl
@@ -0,0 +1,23 @@
+//need to specify how "precise" float should be
+precision mediump float;
+
+varying vec3 v_normalVec;
+varying vec3 v_cameraRayVec;
+
+uniform bool u_useReflection;
+
+uniform samplerCube u_texCube;
+
+//entry point again
+void main() {
+  vec3 normalVec = normalize(v_normalVec);
+	vec3 cameraRayVec = normalize(v_cameraRayVec);
+
+  vec3 texCoords;
+  if(u_useReflection)
+  		texCoords = vec3(0,0,0);
+  else
+  		texCoords = cameraRayVec;
+
+  gl_FragColor = vec4(0,0,0,0);
+}
